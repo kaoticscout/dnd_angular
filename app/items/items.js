2d1a0995d6536d8d1ad740794440603cc58ef8a6
Aaron Klein <aaronrklein@gmail.com>
2016-07-11 07:49:32 -0700

    added magical and valuable items to the items list

--- app/items/items.js
+++ app/items/items.js
@@ -1,5 +1,24 @@
 'use strict';
 
+function getItemText(item){
+  var itemText = "";
+  for (let text of item.text) {
+    if(text != null) {
+      if(text.includes('Source:')) {
+        item.source  = text.substring(text.indexOf(':'), text.length);
+      }
+      else {
+        if(item.name == 'Dawnbringer') {
+          console.log(text)
+        }
+        itemText += " " + text
+      }
+    }
+  }
+  item.text = itemText
+  return item;
+}
+
 angular.module('myApp.items', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,8 +28,30 @@ angular.module('myApp.items', ['ngRoute'])
   });
 }])
 
-.controller('ItemsController', ['$scope', 'items', function($scope, items) {
+.controller('ItemsController', ['$scope', 'items', 'items_valuable', 'items_magic', function($scope, items, items_valuable, items_magic) {
+  $scope.items = []
+
   items.success(function(data) {
-    $scope.items = data;
+    for (let item of data) {
+      item = getItemText(item);
+        $scope.items.push(item);
+    }
   });
+  items_magic.success(function(data) {
+    for (let item of data) {
+      item.value = '???';
+      item = getItemText(item);
+        $scope.items.push(item);
+    }
+  });
+  items_valuable.success(function(data) {
+    for (let item of data) {
+      var itemValue = item.name.substring(0, item.name.indexOf('-')-1);
+      item.name = item.name.substring(item.name.indexOf('-')+1, item.name.length);
+      item.value = itemValue;
+      item = getItemText(item);
+      $scope.items.push(item);
+    }
+  });
+
 }]);
--- app/services/items.js
+++ app/services/items.js
@@ -1,3 +1,12 @@
+app.factory('items', ['$http', function($http) {
+  return $http.get('https://s3-us-west-2.amazonaws.com/melody-s3/data/items/mundane_items.json')
+     .success(function(data) {
+       return data;
+     })
+     .error(function(data) {
+       return data;
+     });
+}]);
 app.factory('items_magic', ['$http', function($http) {
   return $http.get('https://s3-us-west-2.amazonaws.com/melody-s3/data/items/magic_items.json')
      .success(function(data) {
@@ -9,15 +18,6 @@ app.factory('items_magic', ['$http', function($http) {
 }]);
 app.factory('items_valuable', ['$http', function($http) {
   return $http.get('https://s3-us-west-2.amazonaws.com/melody-s3/data/items/valuable_items.json')
-     .success(function(data) {
-       return data;
-     })
-     .error(function(data) {
-       return data;
-     });
-}]);
-app.factory('items', ['$http', function($http) {
-  return $http.get('https://s3-us-west-2.amazonaws.com/melody-s3/data/items/mundane_items.json')
      .success(function(data) {
        return data;
      })
